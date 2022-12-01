7c08e16332ea073c3b22aa273c586a70a7235d7a
Axel Vanherle <axelvanherle@outlook.com>
2022-12-01 13:00:54 +0100

    Switched from inline definition to a cpp file

--- oxoInCPP/PlayGame.h
+++ oxoInCPP/PlayGame.h
@@ -12,38 +12,11 @@ using namespace std;
 class PlayGame
 {
 public:
-    PlayGame()
-    {
-        cout << "Game started." << endl;
-    }
+    PlayGame();
 
-    void setPlayerValues(void)
-    {
-        string newName;
-        char newChar;
-
-        cout << "Player 1 name?: ";
-        cin >> newName;
-        player1.setPlayerName(newName);
-        cout << endl << "Speler 1 char?: ";
-        cin >> newChar;
-        player1.setplayerChar(newChar);
-
-        cout << endl <<  "Speler 2 naam?: ";
-        cin >> newName;
-        player2.setPlayerName(newName);
-        cout << endl << "Speler 2 char?: ";
-        cin >> newChar;
-        player2.setplayerChar(newChar);
-
-        cout << endl << endl << "Player values set." << endl;
-    }
-
-    void getPlayerValues(void)
-    {
-        cout << endl << "Player 1 name: " << player1.getPlayerName() << " char: " << player1.getplayerChar() << endl;
-        cout << endl << "Player 2 name: " << player2.getPlayerName() << " char: " << player2.getplayerChar() << endl;
-    }
+    // Getter and Setter.
+    void setPlayerValues(void);
+    void getPlayerValues(void);
 
 private:
     HumanPlayer player1;
