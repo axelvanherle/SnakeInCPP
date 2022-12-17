bd855e028dab755af3babe509b44e5ffbf5fe06d
Axel Vanherle <axelvanherle@outlook.com>
2022-12-17 14:52:53 +0100

    syntax fix

--- ticTacToeInCPP/playingfield.cpp
+++ ticTacToeInCPP/playingfield.cpp
@@ -67,19 +67,16 @@ int PlayingField::checkTicTacToe(void)
     if (((field[0][0] == field[0][1]) && (field[0][1] == field[0][2]) && (field[0][0] == field[0][2]) && field[0][1] != ' ') || ((field[1][0] == field[1][1]) && (field[1][1] == field[1][2]) && (field[1][0] == field[1][2]) && field[1][1] != ' ') || ((field[2][0] == field[2][1]) && (field[2][1] == field[2][2]) && (field[2][0] == field[2][2]) && field[2][1] != ' '))
     {
         return 0;
-        cout << "SUCCES HORIZONTAL" << endl;
     }
     // Check if vertical tictactoe
     else if (((field[0][0] == field[1][0]) && (field[1][0] == field[2][0]) && (field[0][0] == field[2][0]) && field[1][0] != ' ') || ((field[0][1] == field[1][1]) && (field[1][1] == field[2][1]) && (field[0][1] == field[2][1]) && field[1][1] != ' ') || ((field[0][2] == field[1][2]) && (field[1][2] == field[2][2]) && (field[0][2] == field[2][2]) && field[1][2] != ' '))
     {
         return 0;
-        cout << "SUCCES VERTICAL" << endl;
     }
     // Check if cross tictactoe
     else if ((((field[0][0] == field[1][1]) && (field[1][1] == field[2][2]) && (field[0][0] == field[2][2])) && field[1][1] != ' ') || (((field[0][2] == field[1][1]) && (field[1][1] == field[2][0]) && (field[0][2] == field[2][0])) && field[1][1] != ' '))
     {
         return 0;
-        cout << "SUCCES CROSS" << endl;
     }
     else
     {
